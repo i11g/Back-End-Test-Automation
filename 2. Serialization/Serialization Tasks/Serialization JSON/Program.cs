a76632a085596e7d1bf25376d244b7dd914435d0
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-04-03 21:57:06 +0300

    feat:new files added

---
+++ 2. Serialization/Serialization Tasks/Serialization JSON/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Serialization_JSON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //WeatherForecast weatherForecast = new WeatherForecast()
+            //{
+            //    Date = DateTime.Now,
+            //    TemperatureC = 35,
+            //    Summary="It is a hot day"
+            //};
+
+            //string weatherForecastJson=JsonSerializer.Serialize(weatherForecast);
+
+            //Console.WriteLine(weatherForecastJson);
+
+            //string jsonString = File.ReadAllText("C:\\Users\\Admin\\Desktop\\Back End" +
+            //    " Automation\\Serialization Tasks\\Serialization JSON\\Weather.json");
+
+            //string jsonString = File.ReadAllText(Path.Combine(Environment.CurrentDirectory + "/../../../Weather.json"));
+
+            //var weatherObject=JsonSerializer.Deserialize<List<WeatherForecast>>(jsonString);
+
+            //Console.WriteLine(weatherObject);
+
+            
+
+
+
+        }
+    }
+}
