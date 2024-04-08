17b01e839ae702ccda96320ae349dae61ff3f75b
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-04-08 06:35:10 +0300

    files added

---
+++ 4. Integration Testing with Web API EventMi/EventMi.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace EventMi.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
