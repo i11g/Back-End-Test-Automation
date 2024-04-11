cb4a6f843175acea3c07db1727886aee257696c0
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-04-11 05:58:05 +0300

    new files added

--- 3. Integration Testing of Web API Controllers - EventMi/EventMi.Tests/EventMiTest.cs
+++ 3. Integration Testing of Web API Controllers - EventMi/EventMi.Tests/EventMiTest.cs
@@ -1,3 +1,4 @@
+using Eventmi.Core.Models.Event;
 using RestSharp;
 using System.Net;
 
@@ -43,5 +44,32 @@ namespace EventMi.Tests
             //Assert
             Assert.That(reponse.StatusCode, Is.EqualTo(HttpStatusCode.OK));
         }
+
+        [Test] 
+
+        public async Task Add_PostRequest_AddsNewEventAndRedirect()
+        {
+            //Arrange
+            var input = new EventFormModel()
+            {
+                Name = "DitCome",
+                Place = "Sofia",
+                Start = new DateTime(2024, 10, 10, 10, 0, 0),
+                End = new DateTime(2024, 10, 10, 20, 0, 0)
+            };
+
+            var request = new RestRequest("/Event/All", Method.Post);
+            request.AddHeader("Content-Type", "applocation/x-www-form-urlendcoded");
+            request.AddParameter("Name", input.Name);
+            request.AddParameter("Place", input.Place);
+            request.AddParameter("Start", input.Start.ToString("MM/dd/yy/ hh:mm:ss"));
+            request.AddParameter("End",input.End.ToString("MM/dd/yy/ hh:mm:ss"));
+
+            //Act
+            var response = await _client.ExecuteAsync(request);
+
+            //Assert
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        } 
     }
 }
